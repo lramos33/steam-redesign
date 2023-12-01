55245caa4319b8990273baf99aaa8e6142af8307
lramos33 <oliveira.leonardo3004@gmail.comm>
2023-12-01 00:51:26 -0300

    chore: add styles and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
-import { Inter } from "next/font/google";
-
 import "../styles/globals.css";
+import "../styles/theme.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { MotivaSans } from "@/styles/fonts";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={MotivaSans.className}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,6 @@ const config: Config = {
         "8xl": "1440px",
       },
 
-      spacing: {
-        4.5: "1.125rem",
-        5.5: "1.375rem",
-      },
-
       screens: {
         xs: "460px",
         sm: "576px",
@@ -20,6 +15,27 @@ const config: Config = {
         lg: "1024px",
         xl: "1280px",
         "2xl": "1440px",
+      },
+
+      backgroundColor: {
+        "background-main": "var(--bg-main)",
+        "background-highlight": "var(--bg-highlight)",
+        "background-hover": "var(--bg-hover)",
+        "background-secondary": "var(--bg-secondary)",
+        "background-tertiary": "var(--bg-tertiary)",
+      },
+
+      colors: {
+        "color-primary": "var(--color-primary)",
+        "color-secondary": "var(--color-secondary)",
+        "color-accent-green": "var(--color-accent-green)",
+        "color-accent-red": "var(--color-accent-red)",
+        "color-accent-yellow": "var(--color-accent-yellow)",
+      },
+
+      textColor: {
+        main: "var(--text-main)",
+        dim: "var(--text-dim)",
       },
     },
   },
