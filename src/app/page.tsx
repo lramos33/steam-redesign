2fa4aa01f2c7892a8c9f765a86bd58945a0f8049
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2023-12-01 20:08:28 -0300

    chore: test font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,11 @@ export default function Page() {
     <div className="from-14% to-28% flex flex-1 flex-col items-center bg-gradient-to-b from-[#00030d] to-[#0e141b]">
       <div className="mb-64 flex flex-col gap-16">
         <Image src={HomeGif} alt="home" width={1440} className="h-[468px] object-cover" />
+        <div>
+          <p className="text-xl font-bold text-main">Lorem ipsum</p>
+          <p className="text-xl text-main">Lorem ipsum</p>
+        </div>
+
         <div className="h-80 w-full bg-slate-500"></div>
         <div className="h-80 w-full bg-slate-500"></div>
         <div className="h-80 w-full bg-slate-500"></div>
