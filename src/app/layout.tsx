007ed580f4ac332142c647c0b25cdeed1b92420e
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2023-12-01 01:55:53 -0300

    feat: add header, footer and home banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
+import { cn } from "@/helpers/tailwind.helper";
 import "../styles/globals.css";
 import "../styles/theme.css";
 
+import { Header } from "@/components/ui/Header";
+
 import { MotivaSans } from "@/styles/fonts";
+import { Footer } from "@/components/ui/Footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={MotivaSans.className}>{children}</body>
+      <body className={cn(MotivaSans.className, "flex min-h-screen flex-col")}>
+        <Header />
+        <main className="flex flex-1">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
