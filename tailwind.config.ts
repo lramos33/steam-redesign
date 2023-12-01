c7e8446ccb04247d9f18f4e42b23a11dffa2577b
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2023-12-01 00:04:19 -0300

    initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      maxWidth: {
+        "8xl": "1440px",
+      },
+
+      spacing: {
+        4.5: "1.125rem",
+        5.5: "1.375rem",
+      },
+
+      screens: {
+        xs: "460px",
+        sm: "576px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
       },
     },
   },
-  plugins: [],
-}
-export default config
+};
+
+export default config;
