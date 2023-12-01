d8523b79ce9e5ca1a05d70b1801133d21221ba66
lramos33 <oliveira.leonardo3004@gmail.comm>
2023-12-01 01:55:53 -0300

    feat: add header, footer and home banner

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+import Logo from "../../../public/logo.svg";
+
+export function Footer() {
+  return (
+    <footer className="flex justify-center bg-background-highlight">
+      <div className="mb-24 mt-12 flex h-fit w-full max-w-8xl justify-between">
+        <div className="space-y-8">
+          <Image src={Logo} alt="home" className="h-14" />
+          <p className="max-w-[360px] text-sm text-dim">
+            © 2023 Valve Corporation. All rights reserved. <br />
+            All trademarks are property of their respective owners in the US and other countries. <br />
+            VAT included in all prices where applicable.
+          </p>
+        </div>
+
+        <div className="flex gap-32 text-main">
+          <div className="flex flex-col gap-4">
+            <p>About Valve</p>
+            <p>Jobs</p>
+            <p>Steam works</p>
+            <p>Steam Distribution</p>
+            <p>Support</p>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <p>Privacy Policy</p>
+            <p>Legal</p>
+            <p>Steam Subscriber Agreement</p>
+            <p>Refunds</p>
+            <p>Cookies</p>
+          </div>
+        </div>
+
+        <div className="flex gap-4">
+          <p className="flex h-12 w-12 items-center justify-center rounded bg-background-hover text-2xl text-dim">f</p>
+          <p className="flex h-12 w-12 items-center justify-center rounded bg-background-hover text-2xl text-dim">X</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header className="flex h-14 items-center gap-2 bg-background-tertiary px-4 text-main">
+      <button>Home</button>
+      <button>Browse</button>
+      <button>Discover</button>
+      <button>Points Shop</button>
+      <button>Curatos</button>
+      <button>Gift Cards</button>
+      <button>News</button>
+    </header>
+  );
+}
