c1fe1f19c8399707a219e59be8f6e88ad4dcb8e9
lramos33 <oliveira.leonardo3004@gmail.comm>
2023-12-04 02:31:23 -0300

    refac: header component

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,13 +1,39 @@
+"use client";
+
+import { Link, usePathname } from "@/lib/next";
+
+import { Toggle } from "../primitives/Toggle";
+
+import { SteamLogo } from "../vectors/SteamLogo";
+
 export function Header() {
+  const pathname = usePathname();
+
   return (
-    <header className="flex h-14 items-center gap-2 bg-background-tertiary px-4 text-main">
-      <button>Home</button>
-      <button>Browse</button>
-      <button>Discover</button>
-      <button>Points Shop</button>
-      <button>Curatos</button>
-      <button>Gift Cards</button>
-      <button>News</button>
+    <header className="flex items-center bg-background-main px-3 py-4.5">
+      <div className="flex items-center gap-8">
+        <SteamLogo height="24" fill="#f3f3f3" />
+
+        <Link href="/store/home">
+          <Toggle variant="text" pressed={pathname.startsWith("/store")}>
+            STORE
+          </Toggle>
+        </Link>
+
+        <Toggle variant="text" pressed={pathname.startsWith("/community")}>
+          COMMUNITY
+        </Toggle>
+
+        <Toggle variant="text" pressed={pathname.startsWith("/library")}>
+          LIBRARY
+        </Toggle>
+
+        <Toggle variant="text" pressed={pathname.startsWith("/downloads")}>
+          DOWNLOADS
+        </Toggle>
+      </div>
+
+      <div></div>
     </header>
   );
 }
