baa3367b1c27b15592c7a9e85b8f8ad7d55d9b59
lramos33 <oliveira.leonardo3004@gmail.comm>
2023-12-04 17:08:44 -0300

    chore: add some links and discover page

--- src/components/ui/StoreHeader.tsx
+++ src/components/ui/StoreHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname } from "@/lib/next";
+import { Link, usePathname } from "@/lib/next";
 
 import { Icons } from "../others/Icons";
 import { Toggle } from "../primitives/Toggle";
@@ -11,9 +11,16 @@ export function StoreHeader() {
   return (
     <header className="flex items-center justify-between bg-background-tertiary px-3 py-1.5 text-main">
       <div className="flex gap-1.5">
-        <Toggle pressed={pathname.startsWith("/store/home")}>Home</Toggle>
+        <Link href="/store/home">
+          <Toggle pressed={pathname.startsWith("/store/home")}>Home</Toggle>
+        </Link>
+
         <Toggle pressed={pathname.startsWith("/store/browse")}>Browse</Toggle>
-        <Toggle pressed={pathname.startsWith("/store/discover")}>Discover</Toggle>
+
+        <Link href="/store/discover">
+          <Toggle pressed={pathname.startsWith("/store/discover")}>Discover</Toggle>
+        </Link>
+
         <Toggle pressed={pathname.startsWith("/store/points-shop")}>Points Shop</Toggle>
         <Toggle pressed={pathname.startsWith("/store/curators")}>Curators</Toggle>
         <Toggle pressed={pathname.startsWith("/store/gift-cards")}>Gift Cards</Toggle>
