eff8befbf3cdd577842a48245bbf499ae29957c6
lramos33 <oliveira.leonardo3004@gmail.comm>
2023-12-04 02:31:36 -0300

    chore: minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
-import { cn } from "@/helpers/tailwind.helper";
 import "../styles/globals.css";
 import "../styles/theme.css";
 
+import { Footer } from "@/components/ui/Footer";
 import { Header } from "@/components/ui/Header";
 
 import { MotivaSans } from "@/styles/fonts";
-import { Footer } from "@/components/ui/Footer";
+
+import { cn } from "@/helpers/tailwind.helper";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={cn(MotivaSans.className, "flex min-h-screen flex-col")}>
         <Header />
-        <main className="flex flex-1">{children}</main>
+        <main className="flex flex-1 flex-col">{children}</main>
         <Footer />
       </body>
     </html>
