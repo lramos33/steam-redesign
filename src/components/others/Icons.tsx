9ebfbb9ebdf5fcbaa968b9b570507b359464e030
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2023-12-05 01:43:13 -0300

    chore: add some icons

--- src/components/others/Icons.tsx
+++ src/components/others/Icons.tsx
@@ -1,8 +1,17 @@
 import { LuShoppingCart, LuHeart, LuSettings, LuSearch } from "react-icons/lu";
+import { FaSteam, FaWindows, FaApple } from "react-icons/fa";
+import { MdThumbDown, MdThumbUp } from "react-icons/md";
 
 export const Icons = {
   LuShoppingCart,
   LuHeart,
   LuSettings,
   LuSearch,
+  // ================================== //
+  MdThumbDown,
+  MdThumbUp,
+  // ================================== //
+  FaSteam,
+  FaWindows,
+  FaApple,
 };
