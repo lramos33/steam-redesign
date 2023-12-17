9770a7751e85485580227e99f5aba05aaaeb375b
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2023-12-17 18:42:38 -0300

    chore: add more icons

--- src/components/others/Icons.tsx
+++ src/components/others/Icons.tsx
@@ -1,5 +1,21 @@
 import { MdThumbDown, MdThumbUp } from "react-icons/md";
-import { LuShoppingCart, LuHeart, LuSettings, LuSearch, LuChevronRight, LuChevronLeft } from "react-icons/lu";
+import {
+  LuShoppingCart,
+  LuHeart,
+  LuSettings,
+  LuSearch,
+  LuChevronRight,
+  LuChevronLeft,
+  LuMailCheck,
+  LuUser,
+  LuGamepad2,
+  LuBell,
+  LuUsers,
+  LuBadgeDollarSign,
+  LuChevronDown,
+  LuUserPlus,
+  LuChevronUp,
+} from "react-icons/lu";
 import { FaXTwitter, FaFacebookF, FaSteam, FaWindows, FaApple } from "react-icons/fa6";
 
 export const Icons = {
@@ -7,7 +23,16 @@ export const Icons = {
   LuHeart,
   LuSettings,
   LuSearch,
+  LuUser,
+  LuBell,
+  LuUsers,
+  LuGamepad2,
+  LuMailCheck,
   LuChevronRight,
+  LuBadgeDollarSign,
+  LuUserPlus,
+  LuChevronDown,
+  LuChevronUp,
   LuChevronLeft,
   // ================================== //
   MdThumbDown,
