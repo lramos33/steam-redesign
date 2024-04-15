a38a22bab5ec126bb69922bd8b94eda90d7c3653
lramos33 <oliveira.leonardo3004@gmail.com>
2024-04-15 18:10:15 -0300

    chore: add sticky header

--- src/components/layout/StoreHeader.tsx
+++ src/components/layout/StoreHeader.tsx
@@ -11,7 +11,7 @@ export function StoreHeader() {
   const pathname = usePathname();
 
   return (
-    <header className="flex items-center justify-between bg-background-tertiary px-3 py-1.5">
+    <header className="sticky top-14 z-[999] flex items-center justify-between bg-background-tertiary px-3 py-1.5">
       <div className="flex gap-1.5">
         <Link href="/store/home">
           <Toggle pressed={pathname.startsWith("/store/home")} className="hover:bg-white hover:bg-opacity-10">
