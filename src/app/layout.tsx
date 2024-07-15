35371e4c0d2f10e3e80b5b23269d3b824947deee
lramos33 <oliveira.leonardo3004@gmail.com>
2024-07-15 10:18:01 -0300

    chore: add favicon and metatag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,7 @@ import "swiper/css/navigation";
 import "../styles/globals.css";
 import "../styles/theme.css";
 
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Metadata } from "next";
 
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
@@ -14,6 +13,13 @@ import { Header } from "@/components/layout/Header";
 import { MotivaSans } from "@/styles/fonts";
 
 import { cn } from "@/helpers/tailwind.helper";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Welcome to Steam",
+    template: "%s | Steam",
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -23,8 +29,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main className="hidden flex-1 flex-col 2xl:flex">{children}</main>
         <main className="flex flex-1 items-center justify-center bg-background-main 2xl:hidden">Only available for +1440px screens</main>
         <Footer />
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
